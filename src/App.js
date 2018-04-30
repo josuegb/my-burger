54abae21ec5b21c13aa76d6f89c23418913c91b7
josuegb15 <josueg@thehangar.cr>
2018-04-30 12:26:10 -0600

    Layout Component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React, { Component } from 'react';
+import Layout from './components/Layout/Layout'
+
 class App extends Component {
   render() {
     return (
-      <div>
-      </div>
+      <Layout>
+        <h1>Test</h1>
+        <p>Perro</p>
+      </Layout>
     );
   }
 }
