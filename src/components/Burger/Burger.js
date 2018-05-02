6538674efb07b256447b01a325e73c9381a0be63
josuegb15 <josueg@thehangar.cr>
2018-05-01 18:23:52 -0600

    Burger Component

---
+++ src/components/Burger/Burger.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import classes from './Burger.css'
+import BurgerIngredient from './BurgerIngredient/BurgerIngredient'
+
+
+const burger = (props) => {
+
+  const trasformedIngredients = Object.keys(props.ingredients).map(igkey =>{
+    return [...Array(props.ingredients[igkey])].map((_, i) => {
+      return <BurgerIngredient key={igkey+i} type={igkey} />
+    });
+  });
+
+  return (
+    <div className={classes.Burger}>
+      <BurgerIngredient type="bread-top" />
+      {trasformedIngredients}
+      <BurgerIngredient type="bread-bottom" />
+    </div>
+  );
+}
+
+export default burger;
